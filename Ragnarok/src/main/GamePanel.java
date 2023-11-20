07403626d6c53015de9fb25119b5830217b86309
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-11-20 21:31:27 +0000

    Finish part 1 of the game

---
+++ Ragnarok/src/main/GamePanel.java
@@ -0,0 +1,201 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Entity;
+import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable{ //GamePanel is a child of JPanel. implements Implements an interface. add this interface in UML class diagram.
+
+	// Screen Settings
+	final int originalTileSize = 16; // final --> constant; this is 16x16 tile. Determines the size of the player, npc, everything. possibly why this is 16 bit game
+	// let us use 32x32 later...
+	final int  scale = 3; //scaling it up so we can see it
+	//can adjust the scale if required
+	public final int tileSize = originalTileSize * scale; // now 48x48
+	//can change the below things
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12; // 3:4 ratio
+	public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+	public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	// WORLD SETTINGS
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+//	public final int worldWidth = tileSize * maxWorldCol; // uselesss for now
+//	public final int worldHeight = tileSize * maxWorldRow; // useless for now
+	
+	// FPS
+	int FPS = 60; // can change if necessary
+	
+	// Background Tiles
+	TileManager tileM = new TileManager(this);
+	
+	// Movement
+	KeyHandler keyH = new KeyHandler(); //this handles the keypresses. see KeyHandler.java for details 
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
+	
+	// SOUND 
+	Sound music = new Sound();
+	Sound soundEffect = new Sound();
+
+	
+	Thread gameThread; //gives the game the concept of time... Thread keeps running until you stop it.
+	
+	// UI
+	public UI ui = new UI(this);
+	
+	// PLAYER AND OBJECT
+	public Player player = new Player(this, keyH); // Instantiate Player class
+	public SuperObject obj[] = new SuperObject[10];
+	
+	
+	//now we create the constructor 
+	public GamePanel() {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // this function is inherited from JPanel
+		this.setBackground(Color.black); //can experiment with this 
+		this.setDoubleBuffered(true); // draws whatever comes out of 'this' class in an off screen painting buffer. good for improving rendering performance of the game.
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+	
+	public void setupGame() {
+		aSetter.setObject();
+		
+		playMusic(0);
+	}
+	
+	
+	public void startGameThread()
+	{
+		gameThread = new Thread(this); //this means this class. we are instantiating this thread
+		gameThread.start();
+	}
+
+	@Override
+//	public void run() {  // here we will create a game loop
+//		
+//		// gameLoop methodology: 1. Sleep method
+//		double drawInterval = 1000000000/FPS; // 1 second/60 for 60 frames per second. unit used here is nano seconds. result ~ 0.0166 second
+//		double nextDrawTime = System.nanoTime() + drawInterval;
+//		
+//		while (gameThread != null) {
+//		
+//			// 1. UPDATE: Update information such as character position
+//			update();
+//			
+//			// 2. DRAW: Draw the screen using the updated information.
+//			repaint(); //That's how you call the paintComponent() method. idk why
+//			
+//			// Now some time must have passed. we have to sleep for the remaining time until the next draw time
+//			
+//			try {
+//				
+//				double remainingTime = nextDrawTime - System.nanoTime();
+//				remainingTime /= 1000000; // because sleep only accepts millis 
+//				if (remainingTime < 0) remainingTime = 0; // this is incase update and repaint take longet than the total time between redraws
+//				Thread.sleep((long) remainingTime);
+//				
+//				// now that the thread has been awakened, we need to set the next draw time
+//				
+//				nextDrawTime += drawInterval;
+//				
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		
+//		}
+//	} // this is laggy
+	
+	public void run() {
+		
+		// 2. Delta/Accumulator method
+		
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		// To display FPS
+		long timer = 0;
+		int drawCount = 0;
+		
+		while (gameThread != null)
+		{
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			
+			lastTime = currentTime;
+			
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			
+			if (timer >= 1000000000) {
+				System.out.println("FPS: " + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+		}
+	} // also laggy
+	
+	public void update() {
+		
+		player.update();
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		// TILE
+		tileM.draw(g2);
+		
+		// OBJECT
+		for(int i = 0; i < obj.length; i++) {
+			if (obj[i] != null) {
+				obj[i].draw(g2, this);
+			}
+		}
+		
+		// PLAYER
+		player.draw(g2);
+		
+		// UI
+		ui.draw(g2);
+		
+		g2.dispose();
+	}
+	
+	public void playMusic(int i) {
+		music.setFile(i);
+		music.play();
+		music.loop();
+	}
+	
+	public void stopMusic() {
+		music.stop();
+	}
+	
+	public void playSE(int i) {
+		soundEffect.setFile(i);
+		soundEffect.play();
+	}
+}
---
+++ Ragnarok/src/main/UI.java
@@ -0,0 +1,101 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+
+import object.OBJ_Key;
+
+public class UI {
+	GamePanel gp;
+	Font arial_40, arial_80B;
+	BufferedImage keyImage;
+	public boolean messageOn = false;
+	public String message = "";
+	int messageCounter = 0;
+	public boolean gameFinished = false;
+	
+	double playTime;
+	DecimalFormat dFormat = new DecimalFormat("#0.00");
+	
+	public UI(GamePanel gp) {
+		this.gp = gp;
+		this.arial_40 = new Font("Arial", Font.PLAIN, 40);
+		this.arial_80B = new Font("Arial", Font.BOLD, 80);
+		OBJ_Key key = new OBJ_Key();
+		keyImage = key.image;
+	}
+	
+	public void showMessage(String text) {
+		
+		message = text;
+		messageOn = true;
+	}
+	
+	public void draw(Graphics2D g2) {
+		
+		if (gameFinished) {
+			
+			String text;
+			int textLength;
+			int x;
+			int y;
+			
+			g2.setFont(arial_40);
+			g2.setColor(Color.white);
+			text = "You beat the game!";
+			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			x = gp.screenWidth/2 - textLength/2;
+			y = gp.screenHeight/2 - (gp.tileSize*3);
+			g2.drawString(text, x, y);
+			
+			g2.setFont(arial_40);
+			g2.setColor(Color.white);
+			text = "Your time was: " + dFormat.format(playTime) + "!";
+			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			x = gp.screenWidth/2 - textLength/2;
+			y = gp.screenHeight/2 + (gp.tileSize*4);
+			g2.drawString(text, x, y);
+			
+			g2.setFont(arial_80B);
+			g2.setColor(Color.yellow);
+			text = "Congratulations!";
+			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			x = gp.screenWidth/2 - textLength/2;
+			y = gp.screenHeight/2 + (gp.tileSize*2);
+			g2.drawString(text, x, y);
+			
+			gp.gameThread = null;
+			
+		}
+		
+		else {
+			g2.setFont(arial_40);
+			g2.setColor(Color.white);
+			g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+			g2.drawString("x "+gp.player.hasKey, 74, 65); // this y refers to bottom of the text while for images, it refers to the top.
+			
+			// TIME
+			playTime += (double)1/60;
+			g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize*11, 65);
+			// MESSAGE
+			
+			if (messageOn) {
+				
+				g2.setFont(g2.getFont().deriveFont(30F));
+				g2.drawString(message, gp.tileSize/2, gp.tileSize*5);
+				
+				messageCounter++;
+				
+				if (messageCounter > 120) {
+					messageCounter = 0;
+					messageOn = false;
+				}
+			}
+		}
+		
+		
+	}
+}
